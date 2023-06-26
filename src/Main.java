0e6c731a667f88d6eaae83d757906092d754b8a1
Pavel <pashtetkadet13@gmail.com>
2023-06-26 10:11:47 +0300

    revision

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,7 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(1, 2);
-        int b = calc.multiply.apply(1, 1);
-
-        try {
-            //код, где может произойти деление на ноль
-            int c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        } catch (ArithmeticException e) {
-            //обработка ошибки деления на ноль
-            System.out.println("Ошибка деления на ноль");
-        }
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a, b);
     }
 }
